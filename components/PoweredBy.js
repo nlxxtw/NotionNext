5a3c2b7e82bc3881769655f8b781c056a7a61e92
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-24 18:54:26 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,9 +9,9 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://im.19492035.xyz/file/1742651920554.jpg'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        zdDown {siteConfig('VERSION')}
       </a>
       .
     </div>
