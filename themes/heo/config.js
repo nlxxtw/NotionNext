e46f291b234bba0c3f0e23b4ab38741649245084
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-24 15:27:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -22,8 +22,8 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: 'zdDown一个有温度的资源分享站',
+  HEO_HERO_TITLE_LINK: 'https://im.19492035.xyz/file/1742651920554.jpg',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
