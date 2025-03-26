b7abc4d76ef5277236786f682b85517f7f87a92c
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-26 12:04:51 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -6,6 +6,7 @@ import { siteConfig } from '@/lib/config'
  */
 export default function PoweredBy(props) {
   return (
+/**
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
@@ -15,5 +16,10 @@ export default function PoweredBy(props) {
       </a>
       .
     </div>
+ */
+
+<div class="footer-muted em09"><div class="footer-muted em09"> <div style="text-align: center;">   
+ <a target="_blank" href="https://www.gov.cn/" rel="nofollow"><img style="max-width: 20px; overflow: hidden;" src="https://image.vredu.icu/img/20240825231430.png" alt="">  本站受中华人民共和国法律保护</a>
+<br>采用创作共用版权 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.zh-hans"> CC BY-NC-SA 4.0 CN </a>许可协议<br>
   )
 }
