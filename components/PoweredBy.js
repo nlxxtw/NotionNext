4be412713c625a942a4bf1c0b28072e9dfd4994e
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-26 12:21:21 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,31 +1,29 @@
-/**
+
 import { siteConfig } from '@/lib/config'
 
 export default function PoweredBy(props) {
   return (
 
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
-      <a
-        href='https://im.19492035.xyz/file/1742651920554.jpg'
+      <span className='mr-1'> <a target="_blank" href="https://www.gov.cn/" rel="nofollow"><img style="max-width: 20px; overflow: hidden;" src="https://image.vredu.icu/img/20240825231430.png" alt="">  本站受中华人民共和国法律保护</a></span>
+      <br>
+  <a
+        href='https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.zh-hans'
         className='underline justify-start'>
-        zdDown {siteConfig('VERSION')}
+        采用创作共用版权 {siteConfig('CC BY-NC-SA 4.0 CN')}
       </a>
-      .
+      许可协议<br>
     </div>
  
 
 
   )
 }
- */
-
-import { siteConfig } from '@/lib/config'
 
 /**
- * 驱动版权
- * @returns
- */
+import { siteConfig } from '@/lib/config'
+
+
 export default function PoweredBy(props) {
   return (
     <div className={`footer-muted em09 ${props.className || ''}`}>
@@ -34,7 +32,7 @@ export default function PoweredBy(props) {
           <a target="_blank" href="https://www.gov.cn/" rel="nofollow">
             <img 
               style={{ maxWidth: '20px', overflow: 'hidden' }} 
-              src="https://image.vredu.icu/img/20240825231430.png" 
+              src="https://im.19492035.xyz/file/1742961859726.png" 
               alt=""
             />
             本站受中华人民共和国法律保护
@@ -52,3 +50,4 @@ export default function PoweredBy(props) {
     </div>
   )
 }
+ */
