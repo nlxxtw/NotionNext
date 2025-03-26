331b68bbc62b6b13f8343545c47592921492f287
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-26 12:22:47 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -5,7 +5,7 @@ export default function PoweredBy(props) {
   return (
 
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'> <a target="_blank" href="https://www.gov.cn/" rel="nofollow"><img style="max-width: 20px; overflow: hidden;" src="https://image.vredu.icu/img/20240825231430.png" alt="">  本站受中华人民共和国法律保护</a></span>
+      <span className='mr-1'> <a target="_blank" href="https://www.gov.cn/" rel="nofollow"><img style="max-width: 20px; overflow: hidden;" src="https://im.19492035.xyz/file/1742961859726.png" alt="">  本站受中华人民共和国法律保护</a></span>
       <br>
   <a
         href='https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.zh-hans'
@@ -20,34 +20,4 @@ export default function PoweredBy(props) {
   )
 }
 
-/**
-import { siteConfig } from '@/lib/config'
 
-
-export default function PoweredBy(props) {
-  return (
-    <div className={`footer-muted em09 ${props.className || ''}`}>
-      <div className="footer-muted em09">
-        <div style={{ textAlign: 'center' }}>
-          <a target="_blank" href="https://www.gov.cn/" rel="nofollow">
-            <img 
-              style={{ maxWidth: '20px', overflow: 'hidden' }} 
-              src="https://im.19492035.xyz/file/1742961859726.png" 
-              alt=""
-            />
-            本站受中华人民共和国法律保护
-          </a>
-          <br />
-          采用创作共用版权{' '}
-          <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.zh-hans">
-            CC BY-NC-SA 4.0 CN
-          </a>{' '}
-          许可协议
-          <br />
-          这里面的内容
-        </div>
-      </div>
-    </div>
-  )
-}
- */
