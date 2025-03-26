20fc0b7a3f02a7e75ae0e586c76faf93515a8bf4
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-26 12:14:38 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,12 +1,9 @@
+/**
 import { siteConfig } from '@/lib/config'
 
-/**
- * 驱动版权
- * @returns
- */
 export default function PoweredBy(props) {
   return (
-/**
+
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
@@ -16,10 +13,42 @@ export default function PoweredBy(props) {
       </a>
       .
     </div>
+ 
+
+
+  )
+}
  */
 
-<div class="footer-muted em09"><div class="footer-muted em09"> <div style="text-align: center;">   
- <a target="_blank" href="https://www.gov.cn/" rel="nofollow"><img style="max-width: 20px; overflow: hidden;" src="https://image.vredu.icu/img/20240825231430.png" alt="">  本站受中华人民共和国法律保护</a>
-<br>采用创作共用版权 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.zh-hans"> CC BY-NC-SA 4.0 CN </a>许可协议<br>
+import { siteConfig } from '@/lib/config'
+
+/**
+ * 驱动版权
+ * @returns
+ */
+export default function PoweredBy(props) {
+  return (
+    <div className={`footer-muted em09 ${props.className || ''}`}>
+      <div className="footer-muted em09">
+        <div style={{ textAlign: 'center' }}>
+          <a target="_blank" href="https://www.gov.cn/" rel="nofollow">
+            <img 
+              style={{ maxWidth: '20px', overflow: 'hidden' }} 
+              src="https://image.vredu.icu/img/20240825231430.png" 
+              alt=""
+            />
+            本站受中华人民共和国法律保护
+          </a>
+          <br />
+          采用创作共用版权{' '}
+          <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.zh-hans">
+            CC BY-NC-SA 4.0 CN
+          </a>{' '}
+          许可协议
+          <br />
+          这里面的内容
+        </div>
+      </div>
+    </div>
   )
 }
