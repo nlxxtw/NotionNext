ad477a36f5c9f3cb0f07f94ff4aae59faaad3252
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-27 16:45:06 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -36,9 +36,35 @@ module.exports = {
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: 
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
-    [{"title":"音乐","list":["https://music.163.com/#/playlist?id=779869321"]},{"title":"治愈民谣","list":[""]}],
+    [
     
-  MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || true, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
+     {
+      name: '若月亮没来（若是月亮还没来）',
+      artist: '王宇宙Leto/乔浚丞',
+      url: 'https://pan.19492035.xyz/raw/music/yuelanghaimei.mp3',
+      cover:
+        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+    },
+    {
+      name: '不要慌太阳下山有月光',
+      artist: '小书子',
+      url: 'https://pan.19492035.xyz/raw/music/buyaohuang.mp3',
+      cover:
+        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+    },
+       {
+      name: '一程山路',
+      artist: '毛不易',
+      url: 'https://pan.19492035.xyz/raw/music/yichengshanglu.mp3',
+      cover:
+        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+    }
+    
+    
+    
+    ],
+    
+  MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_SERVER || 'netease', // 音乐平台，[netease, tencent, kugou, xiami, baidu]
   MUSIC_PLAYER_METING_ID:
