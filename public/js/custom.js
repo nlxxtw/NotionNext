060ff6d10470eae9e9854864493270413f9fb379
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-31 12:54:37 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,16 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+// 这里编写自定义js脚本；将被静态引入到页面中
+var OriginTitile = document.title;
+var jiao;
+
+document.addEventListener('visibilitychange', function () {
+		if (document.visibilityState === undefined || document.visibilityState === 'visible') {
+				document.title = '欢迎肥来👀！' + OriginTitile;
+				jiao = setTimeout(function () {
+						document.title = OriginTitile;
+				}, 4000);
+		} else {
+				document.title = 'Where are U🎈;
+				clearTimeout(jiao);
+		}
+});
