ca28a2ca8283bddf82557b8171c2c284effddd10
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-03-31 12:34:05 +0800

    Update memos.js

--- public/js/memos.js
+++ public/js/memos.js
@@ -47,12 +47,12 @@
 
 // Memos Start
 var memo = {
-    host: 'https://memos.chenge.ink/',
-    limit: '20',
+    host: 'https://memos.chenge.ink',
+    limit: '10',
     creatorId: '1',
     domId: '#memos',
-    username: 'zdDown研公办',
-    name: 'zdDown'
+    username: '陈源泉',
+    name: '尘歌'
 }
 if (typeof (memos) !== "undefined") {
     for (var key in memos) {
@@ -371,3 +371,36 @@ function getTotal() {
     });
 };
 window.onload = getTotal();
+// Memos Total End
+
+// Toggle Darkmode
+// const localTheme = window.localStorage && window.localStorage.getItem("theme");
+// const themeToggle = document.querySelector(".theme-toggle");
+
+// if (localTheme) {
+//     document.body.classList.remove("light-theme", "dark-theme");
+//     document.body.classList.add(localTheme);
+// }
+
+// themeToggle.addEventListener("click", () => {
+//     const themeUndefined = !new RegExp("(dark|light)-theme").test(document.body.className);
+//     const isOSDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+//     if (themeUndefined) {
+//         if (isOSDark) {
+//             document.body.classList.add("light-theme");
+//         } else {
+//             document.body.classList.add("dark-theme");
+//         }
+//     } else {
+//         document.body.classList.toggle("light-theme");
+//         document.body.classList.toggle("dark-theme");
+//     }
+
+//     window.localStorage &&
+//         window.localStorage.setItem(
+//             "theme",
+//             document.body.classList.contains("dark-theme") ? "dark-theme" : "light-theme",
+//         );
+// });
+// Darkmode End
