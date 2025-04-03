7816eca300db354381a95ad0c15cb3dbcbfc5e69
zdDown <49474003+nlxxtw@users.noreply.github.com>
2025-04-04 07:41:24 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -13,9 +13,19 @@ const Footer = ({ total }) => {
       {/* 颜色过渡区 */}
       <div className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'/>
 
-      {/* 主要内容容器 */}
+     {/* 主要内容容器 */}
       <div className='bg-white py-8 px-4 mx-auto dark:bg-[#1a191d] dark:border-t dark:border-[#3D3D3F]'>
-      <div className='max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+        <div className='max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+          {/* 联系方式 */}
+          <div>
+            <div className='dark:text-white text-base font-bold mb-8'>联系方式</div>
+            <div className='text-gray-600 dark:text-gray-300 mb-6'>
+              问题反馈：chinavredu@gmail.com
+            </div>
+           /*  <div className='text-gray-600 dark:text-gray-300 mb-6'>
+              微信公众号：zdDown
+            </div>*/
+          </div>
          
 
           {/* 网站信息 */}
@@ -89,8 +99,10 @@ const Footer = ({ total }) => {
       />
     </div>
     <p className='text-gray-600 dark:text-gray-300 text-sm'>官方微信</p>
-  </div>
-</div>
+    </div>
+          </div>
+        </div>
+      </div>
 
       {/* 底部备案信息 */}
       <div className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
